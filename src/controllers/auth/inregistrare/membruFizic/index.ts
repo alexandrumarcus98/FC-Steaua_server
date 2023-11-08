8ae198d128662a789fcf528f2377a3c53f2ecff9
lightview.kali <lightview.kali>
2023-11-08 15:57:13 +0200

    look up, register, email 3

--- src/controllers/auth/inregistrare/membruFizic/index.ts
+++ src/controllers/auth/inregistrare/membruFizic/index.ts
@@ -29,41 +29,40 @@ const inregistrareMembruFizic: any = asyncHandler(async (req, res): Promise<any>
 	const ipinfoWrapper = new IPinfoWrapper("2dcedce28f0ef8", cache);
 	let location = {}
 
-	ipinfoWrapper.lookupIp(req?.ip || req?.socket?.remoteAddress).then((response: IPinfo) => {
-		console.log(response);
+	ipinfoWrapper.lookupIp(req?.ip || req?.socket?.remoteAddress).then(async (response: IPinfo) => {
 		location = response
-	});
-
-	const user = await MembruFizic.create({
-		email: email,
-		parola: hashedPassword,
-		tipAbonament: tipAbonament,
-		data: {
-			ipInfo: res.locals.ipInfo || req.ip || req.socket.remoteAddress,
-			socketInfo: geoip.lookup(res.locals.ipInfo || req.ip || req.socket.remoteAddress),
-			ip: req.ip,
-			socketIp: req.socket.remoteAddress,
-			ua: res.locals.ua,
-			location: location
-		},
-		sex: sex,
-		nrTel: nrTel,
-		dataNasterii: dataNasterii,
-		oras: oras,
-		tara: tara,
-		nume: nume,
-		prenume: prenume,
-		regiune: regiune,
-		membrii: membrii,
-		adresa: adresa,
-	})
-
-	if (user?._id) {
-		return res.status(201).json({
-			userId: user?._id,
-			message: "Cont creat cu success.",
+		const user = await MembruFizic.create({
+			email: email,
+			parola: hashedPassword,
+			tipAbonament: tipAbonament,
+			data: {
+				ipInfo: res.locals.ipInfo || req.ip || req.socket.remoteAddress,
+				socketInfo: geoip.lookup(res.locals.ipInfo || req.ip || req.socket.remoteAddress),
+				ip: req.ip,
+				socketIp: req.socket.remoteAddress,
+				ua: res.locals.ua,
+				location: location
+			},
+			sex: sex,
+			nrTel: nrTel,
+			dataNasterii: dataNasterii,
+			oras: oras,
+			tara: tara,
+			nume: nume,
+			prenume: prenume,
+			regiune: regiune,
+			membrii: membrii,
+			adresa: adresa,
 		})
-	}
+
+		if (user?._id) {
+			return res.status(201).json({
+				userId: user?._id,
+				message: "Cont creat cu success.",
+			})
+		}
+	});
+	return res.status(404).json({ message: "Ceva nu a mers bine..." })
 })
 
 export { inregistrareMembruFizic }
